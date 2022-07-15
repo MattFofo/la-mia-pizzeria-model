e4b8b39e72a49f38f07d43076aa3bd61d95355cd
MattFofo <matteo.segattini2@gmail.com>
2022-07-15 12:22:56 +0200

    Add project files.

---
+++ DataBase/PizzeriaContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.DataBase
+{
+    public class PizzeriaContext : DbContext
+    {
+
+    }
+}
---
+++ Pizza.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+
+        public Pizza(string name, string description, string image, decimal price)
+        {
+            Id = new Random().Next(1, 999999);
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+}
