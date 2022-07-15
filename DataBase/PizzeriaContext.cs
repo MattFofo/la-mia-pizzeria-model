efb2ee096955b23a65ba02ad293a76320ae78f77
MattFofo <matteo.segattini2@gmail.com>
2022-07-15 13:49:21 +0200

    migrations + init db

--- DataBase/PizzeriaContext.cs
+++ DataBase/PizzeriaContext.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace la_mia_pizzeria_static.DataBase
 {
     public class PizzeriaContext : DbContext
     {
+        public DbSet<Pizza> Pizzas { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db-pizzeria;Integrated Security=True");
+        }
     }
 }
